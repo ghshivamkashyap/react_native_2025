093105ca4a733a7ed4b3a9db6a7e232785269576
shivam digiprod <lootforecast@gmail.com>
2025-02-12 23:57:41 +0530

    added functional react component in app.js main file

--- first_app/App.js
+++ first_app/App.js
@@ -1,23 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-
-  console.log("Hello from console");
-  
+const App = () => {
   return (
     <View style={styles.container}>
       <Text>Hello shivam</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#faf",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
